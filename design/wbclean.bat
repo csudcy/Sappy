e8ae5c82287b0723f804c386e5334bef09030030
Nicholas Lee <csudcy@gmail.com>
2015-02-12 23:57:48 +0000

    Renamed files to work on Heroku

--- design/wbclean.bat
+++
@@ -1,5 +0,0 @@
-convert "1. Overall.jpg" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%%,91%%,0.1 "c1. Overall.jpg"
-convert "2. Welcome, Big leader.jpg" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%%,91%%,0.1 "c2. Welcome, Big leader.jpg"
-convert "3. Slim leader & flow.jpg" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%%,91%%,0.1 "c3. Slim leader & flow.jpg"
-convert "4. Participant flow.jpg" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%%,91%%,0.1 "c4. Participant flow.jpg"
-pause
--- meteor.sh
+++ meteor.sh
@@ -9,18 +9,12 @@ if [ ! `command -v meteor 2>/dev/null` ]; then
     curl https://install.meteor.com | sudo sh
 fi
 
-# If the real app doesnt exist, create it
-if [ ! -d /vagrant/supp ]; then
-    meteor create /vagrant/supp
-    mkdir -p /vagrant/supp/.meteor/local
-fi
-
 # Mount a directory which can lock files for mongod
-if [ ! -d /home/vagrant/supp/.meteor/local ]; then
-    mkdir -p /home/vagrant/supp/.meteor/local
+if [ ! -d /home/vagrant/.meteor/local ]; then
+    mkdir -p /home/vagrant/.meteor/local
 fi
-sudo mount --bind /home/vagrant/supp/.meteor/local/ /vagrant/supp/.meteor/local/
+sudo mount --bind /home/vagrant/.meteor/local/ /vagrant/.meteor/local/
 
 # Run the app
-cd /vagrant/supp
+cd /vagrant
 meteor
