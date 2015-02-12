6e59370872fb88065ed90f691a739cb8657716e9
Nicholas Lee <csudcy@gmail.com>
2015-02-12 15:54:42 +0000

    Resources cannot create rooms anymore

--- supp/client/views/welcome/welcome.js
+++ supp/client/views/welcome/welcome.js
@@ -1,10 +1,66 @@
 
+function join(user_type, room, user) {
+    // Save details to the persistent session
+    PersistentSession.set('room', room);
+    if (user_type === 'resource') {
+        PersistentSession.set('user', user);
+    }
+
+    // Make sure the room exists
+    Rooms.upsert(
+        // Selector
+        {
+            _id: room
+        },
+        // Modifier
+        {
+            // If we've inserted the room, make sure it has a users object
+            $setOnInsert: {
+                users: {}
+            }
+        }
+    );
+
+    // Decide what to do
+    if (user_type === 'resource') {
+        // Make sure the user exists in the room
+        var set = {};
+        set['users.'+user] = null;
+        Rooms.update(
+            // Selector
+            {
+                _id: room
+            },
+            // Modifier
+            {
+                $set: set
+            }
+        );
+
+        // Show the next template
+        show_template(Template.resource);
+    } else {
+        // Show the next template
+        show_template(Template.scrum_master);
+    }
+}
+
 Template.welcome.helpers({
     user_type: function (check_user_type) {
         if (check_user_type !== undefined) {
             return PersistentSession.get('user_type') === check_user_type;
         }
         return PersistentSession.get('user_type');
+    },
+    rooms: function() {
+        return Rooms.find();
+    },
+    room_selected: function(room) {
+        // This is a very hacky helper because Meteor can't cope with reactive variables being used in an if statement
+        if (PersistentSession.get('room') == room) {
+            return 'selected';
+        }
+        return '';
     }
 });
 
@@ -24,48 +80,6 @@ Template.welcome.events({
             room = $('.welcome_container [name=room]').val(),
             user = $('.welcome_container [name=user]').val();
 
-        // Save details to the persistent session
-        PersistentSession.set('room', room);
-        if (user_type === 'resource') {
-            PersistentSession.set('user', user);
-        }
-
-        // Make sure the room exists
-        Rooms.upsert(
-            // Selector
-            {
-                _id: room
-            },
-            // Modifier
-            {
-                // If we've inserted the room, make sure it has a users object
-                $setOnInsert: {
-                    users: {}
-                }
-            }
-        );
-
-        // Decide what to do
-        if (user_type === 'resource') {
-            // Make sure the user exists in the room
-            var set = {};
-            set['users.'+user] = null;
-            Rooms.update(
-                // Selector
-                {
-                    _id: room
-                },
-                // Modifier
-                {
-                    $set: set
-                }
-            );
-
-            // Show the next template
-            show_template(Template.resource);
-        } else {
-            // Show the next template
-            show_template(Template.scrum_master);
-        }
+        join(user_type, room, user);
     }
 });
