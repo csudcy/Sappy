9f741b2a946040a761cc9f1514727db7b31f6a26
Nicholas Lee <csudcy@gmail.com>
2015-02-12 12:06:46 +0000

    Save room & name to the persistent session

--- supp/client/views/welcome/welcome.js
+++ supp/client/views/welcome/welcome.js
@@ -16,11 +16,23 @@ Template.welcome.events({
         var user_type = PersistentSession.get('user_type'),
             room = $('.welcome_container [name=room]').val(),
             name = $('.welcome_container [name=name]').val();
+
+        // Save details to the persistent session
+        PersistentSession.set('room', room);
+        PersistentSession.set('name', name);
+
+        // Decide what to do
         if (user_type === 'resource') {
+            // Do some db stuff
             console.log('Resource "'+name+'" joining "'+room+'"!')
+
+            // Show the next template
             show_template(Template.resource);
         } else {
+            // Do some db stuff
             console.log('Master joining "'+room+'"!')
+
+            // Show the next template
             show_template(Template.scrum_master);
         }
     }
