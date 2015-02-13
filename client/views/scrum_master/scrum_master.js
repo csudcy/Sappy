53deca9d30197adc7faaa21fe521f925cd529aff
Fran Smith <fran.smith@artsalliancemedia.com>
2015-02-13 09:28:21 +0000

    made the cards pretty

--- client/views/scrum_master/scrum_master.js
+++ client/views/scrum_master/scrum_master.js
@@ -32,19 +32,20 @@ function finished_vote(users_obj) {
 function get_all_votes(users_obj) {
     var votes = [];
     for(var key in users_obj) {
-        if (users_obj[key] === 'C') {
-            console.log('some chose a coffe break');
-        } else if (users_obj[key] === '?') {
-            console.log('uncertain');
-        } else {
+        if (users_obj[key] !== 'C' || users_obj[key] === '?') {
             votes.push(parseInt(users_obj[key], 10));
         }
     }
     return votes;
 }
 
+function get_fib_numbers() {
+    return [0, 1, 2, 3, 5, 8, 13, 20, 40, 100];
+}
+
 function get_average_number(votes) {
     var sum = 0;
+    console.log(votes)
     for (var vote in votes) {
         console.log(votes[vote]);
         sum += votes[vote];
@@ -54,7 +55,7 @@ function get_average_number(votes) {
 
 function workout_average_fib_number(votes) {
     var average = Math.round(get_average_number(votes)),
-        fibs = [0, 1, 2, 3, 5, 8, 13, 20, 40, 100],
+        fibs = get_fib_numbers(),
         average_fib = 0;
 
     for (var i in fibs) {
@@ -63,6 +64,46 @@ function workout_average_fib_number(votes) {
         }
     }
     return average_fib;
+}
+
+function get_average_position(avg_fib, fibs) {
+    var current_position = 0;
+
+    for (var i in fibs) {
+        if (fibs[i] === avg_fib) {
+            return i;
+        }
+    }
+}
+
+function get_lower_bound(avg_fib_position, fibs) {
+    if (avg_fib_position !== 0) {
+        return fibs[avg_fib_position - 1];
+    }
+    return null;
+}
+
+function get_upper_bound(avg_fib_position, fibs) {
+    if (avg_fib_position !== fibs.length - 1) {
+        return fibs[avg_fib_position + 1];
+    }
+    return null;
+}
+
+function is_extreme_vote(users_obj, vote) {
+    var votes = get_all_votes(users_obj),
+        avg_fib = workout_average_fib_number(votes),
+        fibs = get_fib_numbers(),
+        avg_fib_position = get_average_position(avg_fib, fibs),
+        lower_bound = get_lower_bound(avg_fib_position, fibs),
+        upper_bound = get_upper_bound(avg_fib_position, fibs);
+
+    // if further than one fib number away of the average, it's considdered an extreme
+    if (lower_bound && vote < lower_bound || upper_bound && vote > upper_bound) {
+        return true;
+    }
+
+    return false;
 }
 
 Template.scrum_master.helpers({
@@ -98,12 +139,22 @@ Template.scrum_master.helpers({
             // get the average & display
             return workout_average_fib_number(votes);
         }
-        return 10000000;
     },
     card_state: function(vote) {
         if (get_percentage() == 100) {
             // Voting is over; show the card
-            return 'card_front';
+            var classes = 'card_front';
+
+            // highlight coffee or unknown
+            if (vote === 'C' || vote === '?') {
+                classes += ' highlight_non_vote';
+            }
+            // highlight extreme
+            if (is_extreme_vote(get_users(), vote)) {
+                classes += '  highlight_extreme_vote';
+            }
+            console.log(classes)
+            return classes;
         }
 
         if (vote) {
