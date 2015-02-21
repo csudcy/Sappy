a12c828c6dd73917ee315b950168d22bf287775e
Nicholas Lee <csudcy@gmail.com>
2015-02-21 01:18:14 +0000

    Minor improvements

--- .testing/jquery.fireworks.js
+++ .testing/jquery.fireworks.js
@@ -1,5 +1,10 @@
 /*
 Adapted from http://jsfiddle.net/dtrooper/AceJJ/
+
+TODO:
+ * Try to get rid of ghosting
+ * See if anything can be made more efficient
+ * Make the canvas fit in the z-order
 */
 
 (function( $ ) {
--- .testing/test.js
+++ .testing/test.js
@@ -1,22 +1,12 @@
 setTimeout(function() {
     // $('div').fireworks();
 
-    $('#on_red').click(function() {
-        $('div.red').fireworks();
-    });
-    $('#on_blue').click(function() {
-        $('div.blue').fireworks();
-    });
-    $('#on_both').click(function() {
-        $('div').fireworks();
-    });
-    $('#off_red').click(function() {
-        $('div.red').fireworks('destroy');
-    });
-    $('#off_blue').click(function() {
-        $('div.blue').fireworks('destroy');
-    });
-    $('#off_both').click(function() {
-        $('div').fireworks('destroy');
-    });
+    $('#on_bg').click(function() { $('div.bg').fireworks(); });
+    $('#on_blue').click(function() { $('div.blue').fireworks(); });
+    $('#on_red').click(function() { $('div.red').fireworks(); });
+    $('#on_all').click(function() { $('div').fireworks(); });
+    $('#off_bg').click(function() { $('div.bg').fireworks('destroy'); });
+    $('#off_blue').click(function() { $('div.blue').fireworks('destroy'); });
+    $('#off_red').click(function() { $('div.red').fireworks('destroy'); });
+    $('#off_all').click(function() { $('div').fireworks('destroy'); });
 });
